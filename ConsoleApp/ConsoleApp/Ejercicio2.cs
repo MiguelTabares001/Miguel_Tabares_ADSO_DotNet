e8e2931d856b8002420c21113d0678163b1c76ab
MiguelTabares001 <demisebully@gmail.com>
2024-04-24 21:18:53 -0500

    añadido ejercicios 4 y 5

--- ConsoleApp/ConsoleApp/Ejercicio2.cs
+++ ConsoleApp/ConsoleApp/Ejercicio2.cs
@@ -1,43 +1,35 @@
-﻿using System;
-
-//----------------Ejercicio 2--------------
+﻿//----------------Ejercicio 2--------------
 //- En c# sin usar formularios, crear un programa que determine si una frase o palabra es palindroma. Contabilizar e imprimir numero de vocales, elimina los espacios a la hora de comprar si es palindromo o no, la frase o palabra o numero sea dada por el usuario en la consola. Ejemplo: ala 7007
 
 using System;
 
-public class PalindromeChecker
+public class Palindromo
 {
-    public void Main(string[] args)
+    public static void Main(string[] args)
     {
-        Console.Write("Enter a word or phrase: ");
-        string input = Console.ReadLine().ToLower(); // convertio el coso de entrada en minuscula
 
-        // Quito los espacios para que funcione !!
-        input = input.Replace(" ", "");
+        Console.Write("Ingrese una frase para verificar si es un palindromo: ");
+        string input = Console.ReadLine();
 
-        // Conteo de vocales
-        int vowelCount = 0;
-        foreach (char c in input)
+        // quito espacios -> con esto se arreglo
+        string cleanInput = input.Replace(" ", "").ToLower();
+
+        // Reverso de la cadena
+        char[] charArray = cleanInput.ToCharArray();
+        Array.Reverse(charArray);
+        string reversedInput = new string(charArray);
+
+        // Comprobacion si la cadena original es igual a su reverso
+        bool isPalindrome = cleanInput.Equals(reversedInput);
+
+        if (isPalindrome)
         {
-            if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u')
-            {
-                vowelCount++;
-            }
+            Console.WriteLine("La frase ingresada es un palindromo.");
         }
-
-        // Verifico si es palindromo
-        bool isPalindrome = true;
-        for (int i = 0, j = input.Length - 1; i < input.Length / 2; i++, j--)
+        else
         {
-            if (input[i] != input[j])
-            {
-                isPalindrome = false;
-                break;
-            }
+            Console.WriteLine("La frase ingresada no es un palindromo.");
         }
-
-        // SAlida tactica
-        Console.WriteLine("Number of vowels: " + vowelCount);
-        Console.WriteLine(isPalindrome ? input + " is a palindrome" : input + " is not a palindrome");
     }
 }
+
---
+++ ConsoleApp/ConsoleApp/Ejercicio5.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+//----------Ejercicio 3---------------------- -
+
+/*-Realice un programa en c# que lea unas observaciones y reemplace los caracteres @ | & # por X. 
+Elimine los caracteres ? ¿. 
+Inserte al comienzo la palabra init. 
+Inserte al final la palabra end. 
+Imprimir todo en mayusculas.
+*/
+
+using System;
+
+public class HelloWorld
+{
+    public void Main(string[] args)
+    {
+        // Leer las cosas insertadas por el usuario
+        Console.Write("Inserte su observacion: ");
+        string input = Console.ReadLine();
+
+        // reemplazo @ | & # con una X
+        input = input.Replace("@", "X");
+        input = input.Replace("|", "X");
+        input = input.Replace("&", "X");
+        input = input.Replace("#", "X");
+
+        // eliminacion signos ? ¿
+        input = input.Replace("?", "");
+        input = input.Replace("¿", "");
+
+        // Inserto init al principio
+        input = "init " + input;
+
+        // Insert end al final
+        input = input + " end";
+
+        // Convert to uppercase
+        input = input.ToUpper();
+
+        // Resultados
+        Console.WriteLine("REsultados:");
+        Console.WriteLine(input);
+    }
+}
+
