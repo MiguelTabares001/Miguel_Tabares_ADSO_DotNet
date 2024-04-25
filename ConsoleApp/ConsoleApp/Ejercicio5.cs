794fa5707819790bdb7524dcb7ac4de6f9ff85c4
MiguelTabares001 <140347359+MiguelTabares001@users.noreply.github.com>
2024-04-24 21:29:22 -0500

    ejercicio 5

--- ConsoleApp/ConsoleApp/Ejercicio5.cs
+++ ConsoleApp/ConsoleApp/Ejercicio5.cs
@@ -1,46 +1,26 @@
-﻿using System;
+/* 
+ ----------------Ejercicio 5---------------------- 
 
-//----------Ejercicio 3---------------------- -
-
-/*-Realice un programa en c# que lea unas observaciones y reemplace los caracteres @ | & # por X. 
-Elimine los caracteres ? ¿. 
-Inserte al comienzo la palabra init. 
-Inserte al final la palabra end. 
-Imprimir todo en mayusculas.
+Realice Un programa usando c# que te pida tu nombre y 
+lo muestre en una pantalla separando cada letra de la siguiente con un espacio.
+Ejemplo: si tu nombre es "Maria" debería aparecer en pantalla "M a r i a".´
 */
 
 using System;
 
-public class HelloWorld
+class Program2
 {
-    public void Main(string[] args)
+    static void Main(string[] args)
     {
-        // Leer las cosas insertadas por el usuario
-        Console.Write("Inserte su observacion: ");
-        string input = Console.ReadLine();
+        Console.WriteLine("Por favor, introduce tu nombre:");
+        string nombre = Console.ReadLine();
 
-        // reemplazo @ | & # con una X
-        input = input.Replace("@", "X");
-        input = input.Replace("|", "X");
-        input = input.Replace("&", "X");
-        input = input.Replace("#", "X");
+        // Aqui se itera sobre cada letra del nombre y se imprime con el espacio
+        foreach (char letra in nombre)
+        {
+            Console.Write(letra + " ");
+        }
 
-        // eliminacion signos ? ¿
-        input = input.Replace("?", "");
-        input = input.Replace("¿", "");
-
-        // Inserto init al principio
-        input = "init " + input;
-
-        // Insert end al final
-        input = input + " end";
-
-        // Convert to uppercase
-        input = input.ToUpper();
-
-        // Resultados
-        Console.WriteLine("REsultados:");
-        Console.WriteLine(input);
+        Console.WriteLine(); // Salto de línea al final
     }
 }
-
